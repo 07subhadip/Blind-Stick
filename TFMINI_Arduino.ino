08709892a5e853dd43eacf15bcecc77fd6989e01
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-04 16:10:11 +0530

    init

---
+++ TFMINI_Arduino.ino
@@ -0,0 +1,57 @@
+#include <SoftwareSerial.h>
+#include "TFMini.h"
+TFMini tfmini;
+
+// Adjust these pin numbers to the pins connected to the TFMini
+SoftwareSerial SerialTFMini(2,1); // RX, TX for ESP8266 (e.g., NodeMCU or Wemos D1 Mini)
+
+void getTFminiData(int* distance, int* strength) {
+  static char i = 0;
+  char j = 0;
+  int checksum = 0;
+  static int rx[9];
+  if (SerialTFMini.available()) {
+    rx[i] = SerialTFMini.read();
+    if (rx[0] != 0x59) {
+      i = 0;
+    } else if (i == 1 && rx[1] != 0x59) {
+      i = 0;
+    } else if (i == 8) {
+      for (j = 0; j < 8; j++) {
+        checksum += rx[j];
+      }
+      if (rx[8] == (checksum % 256)) {
+        *distance = rx[2] + rx[3] * 256;
+        *strength = rx[4] + rx[5] * 256;
+      }
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200); // Initialize hardware serial port (serial debug port)
+  Serial.println("Initializing...");
+  SerialTFMini.begin(115200); // Assuming TFMINI_BAUDRATE is 115200
+  tfmini.begin(&SerialTFMini); // Initialize the TF Mini sensor
+}
+
+void loop() {
+  int distance = 0;
+  int strength = 0;
+
+  getTFminiData(&distance, &strength);
+  while (!distance) {
+    getTFminiData(&distance, &strength);
+    if (distance) {
+      Serial.print(distance);
+      Serial.print("cm\t");
+      Serial.print("strength: ");
+      Serial.println(strength);
+    }
+  }
+  delay(100);
+}
+//  THIS WORKS
